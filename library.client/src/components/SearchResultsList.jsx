254515a53533e5374b7c754d045398e54c442415
elexsa <feegee1029384756@gmail.com>
2024-06-05 01:43:07 +0300

    THE FINALIST CLEAN

--- library.client/src/components/SearchResultsList.jsx
+++ library.client/src/components/SearchResultsList.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import "./SearchResultsListStyles.css"
 import { SearchResult } from './SearchResult';
 export const SearchResultsList = ({ results }) =>
-{
-    return (
-        <div className="results-list">
-            {
-                results.map((result, id) =>
+{   
+
+    if (results != null) {
+        return (
+            <div className="results-list">
                 {
-                    return <SearchResult result={result} key={id} />;
-                })
-            }
-        </div>
-    );
+                    results.map((result, id) => {
+                        return <SearchResult result={result} key={id} />;
+                    })
+                }
+            </div>
+        );
+    } else {
+        return (
+            <div className="results-list">
+                <a className="search-result" href="#">
+                   No books found
+                </a>
+            </div>)
+    }
+
 }
